1d50d90f8b9158bf3d966a8032766059dd847568
oopserv <oopserv@gmail.com>
2017-05-17 22:36:02 -0500

    basic mutation

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
@@ -62,7 +63,26 @@ const RootQuery = new GraphQLObjectType({
     }
   }
 })
-//
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve (parentValue, { firstName, age }) {
+        return axios.post('http://localhost:3000/users/', { firstName, age })
+        .then(res => res.data)
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 })
